d8d35106c1d46035f98abab3c9061b7e220d6694
Hafardev <farmanihadis@gmail.com>
2023-07-14 18:38:13 +0330

    25-improvement ui of fragment

--- domain/src/main/java/com/filimo/domain/usecase/BaseUseCase.kt
+++ domain/src/main/java/com/filimo/domain/usecase/BaseUseCase.kt
@@ -4,13 +4,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
-abstract class BaseUseCase<P,Flow>  {
+abstract class BaseUseCase<P,R>  {
 
     var executorDispatcher = Dispatchers.Default
 
-    abstract suspend fun onExecute(param :P ?) :Flow ?
+    abstract suspend fun onExecute(param :P ?) :R ?
 
-   open suspend fun executeHandler (param: P ? ) : Flow ?{
+   open suspend fun executeHandler (param: P ? ) : R ?{
         return withContext(executorDispatcher){
             onExecute(param)
         }
